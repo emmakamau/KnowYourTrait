b4a34367c1109b469467f50a2502929d2e5d3566
jackie <bobbinnej@gmail.com>
2022-03-22 14:09:34 +0300

    Hide quiz until start test button is clicked

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+/*code to hide quiz until start test button is clicked*/
+    $("#start-test").click(function(){
+        $("#quiz-form").show();
+       
+       
+
+    });
+
+});
