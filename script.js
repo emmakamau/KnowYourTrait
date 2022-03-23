91fddb92a34d4bc57120450a3e5aeee740b0f7c4
Charmain <mogerecharmain@gmail.com>
2022-03-23 06:41:37 +0300

    This will add an about us section and a description section as well as images.

--- script.js
+++ script.js
@@ -37,5 +37,15 @@ scrollButton.onclick=()=>{
     document.documentElement.scrollTop=0;
 }
 
-
+let cardTotalNumber =(
+    $(".card").toArray().length
+  );
+  
+  for(let cardNumber = 1; cardNumber<=cardTotalNumber;cardNumber++ ){
+    $(`.card${cardNumber}`).click(function() {
+        $(`.card-show${cardNumber}`).toggle(400);
+        $(`.card-hidden${cardNumber}`).toggle(400);
+      });
+  }
+  
 
