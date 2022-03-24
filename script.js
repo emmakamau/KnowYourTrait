9d3db83eca51da15aab6705a0c050c450684722c
EmmaK <emmaculatewkamau@gmail.com>
2022-03-24 20:06:30 +0300

    Add more functionality to second start btn

--- script.js
+++ script.js
@@ -139,6 +139,10 @@ $(document).ready(function(){
 
     /*code to hide quiz until the second start test button is clicked*/
     $("#start-test2").click(function(){
+        $("#home-section").removeClass("header")
+        $("#home-section").addClass("header-quiz")
+        $("ul#nav-links").removeClass("nav__links")
+        $("ul#nav-links").addClass("nav__links_2")
         $("#quiz-name").show();
         $("#start-test").hide()
         $("#start-test2").hide()
